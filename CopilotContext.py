d5d8ad9fee550761c827f902f879a114a3bb80b1
melionel <mengla@microsoft.com>
2023-07-23 00:06:30 +0800

    support conversation

--- CopilotContext.py
+++ CopilotContext.py
@@ -52,6 +52,8 @@ class CopilotContext:
         self.openai_model = os.environ.get("OPENAI_API_KEY")
 
         self.local_folder = generate_random_folder_name()
+        self.messages = []
+        self.flow_generated = False
 
         env = Environment(loader=FileSystemLoader('./'), variable_start_string='[[', variable_end_string=']]')
         self.template = env.get_template('pfplaner_v2.jinja2')
@@ -100,38 +102,43 @@ class CopilotContext:
             if not self.aoai_key or not self.aoai_deployment or not self.aoai_api_base:
                 return False, "You configured to use AOAI, but one or more of the following environment variables were not set: AOAI_API_KEY, AOAI_DEPLOYMENT, AOAI_API_BASE"
             else:
+                openai.api_key = self.aoai_key
+                openai.api_type = "azure"
+                openai.api_base = self.aoai_api_base
+                openai.api_version = "2023-07-01-preview"
                 return True, ""
         else:
             if not self.openai_key or not self.openai_model:
                 return False, "You configured to use OPENAI, but one or more of the following environment variables were not set: OPENAI_API_KEY, OPENAI_API_KEY"
             else:
+                openai.api_key = self.openai_key
                 return True, ""
-            
-    def ask_gpt(self, content, print_info_func):
-        prompt = self.template.render(goal=content)
-            
-        chat_message = [
-            {'role':'system', 'content':'you are helpful assitant and can help user to write high quality code based on the provided examples'},
-            {'role':'user', 'content':prompt}
-        ]
 
+    def format_request_dict(self, function_call):
         request_args_dict = {
-            "messages": chat_message,
+            "messages": self.messages,
             "functions": self.my_custom_functions,
-            "function_call": {'name': 'dump_flow'},
+            "function_call": function_call,
             "stream": False,
             "temperature": 0
         }
 
         if self.use_aoai:
-            openai.api_key = self.aoai_key
-            openai.api_type = "azure"
-            openai.api_base = self.aoai_api_base
-            openai.api_version = "2023-07-01-preview"
             request_args_dict['engine'] = self.aoai_deployment
         else:
-            openai.api_key = self.openai_key
             request_args_dict['model'] = self.openai_model
+
+        return request_args_dict         
+
+    def generate_flow(self, goal, print_info_func):
+        prompt = self.template.render(goal=goal)
+            
+        self.messages = [
+            {'role':'system', 'content':'you are helpful assitant and can help user to write high quality code based on the provided examples'},
+            {'role':'user', 'content':prompt}
+        ]
+
+        request_args_dict = self.format_request_dict({'name': 'dump_flow'})
 
         print_info_func('Generating prompt flow for your goal, please wait...', 'Copilot')
         response = openai.ChatCompletion.create(**request_args_dict)
@@ -149,5 +156,31 @@ class CopilotContext:
         function_arguments = json.loads(getattr(response.choices[0].message.function_call, "arguments", ""))
         dump_flow(**function_arguments, target_folder=self.local_folder)
 
+        self.flow_generated = True
+        self.messages.append({"role": "assistant", "function_call": response.choices[0].message.function_call, "content": ""})
+
         print_info_func(f'Congratulations! Your prompt flow was successfully dumped to local folder: {self.local_folder}', 'Copilot')
         print_info_func(f'How the flow works: {function_arguments["explaination"]}', 'Copilot')
+
+    def ask_gpt(self, content, print_info_func):
+        if not self.flow_generated:
+            self.generate_flow(content, print_info_func)
+        else:
+            print_info_func('Contact ChatGPT for furthur help, please wait...', 'Copilot')
+            self.messages.append({'role':'user', 'content':content})
+            request_args_dict = self.format_request_dict('auto')
+            
+            response = openai.ChatCompletion.create(**request_args_dict)
+            response_ms = response.response_ms
+            prompt_tokens = response.usage.prompt_tokens
+            completion_tokens = response.usage.completion_tokens
+            total_tokens = response.usage.total_tokens
+
+            message = getattr(response.choices[0].message, "content", "")
+
+            print_info_func(f'Get response from ChatGPT in {response_ms} ms!', 'Copilot')
+            print_info_func(f'total tokens:{total_tokens}\tprompt tokens:{prompt_tokens}\tcompletion tokens:{completion_tokens}', 'Copilot')
+
+            self.messages.append({'role':'user', 'content':message})
+
+            print_info_func(f'{message}', 'Copilot')
