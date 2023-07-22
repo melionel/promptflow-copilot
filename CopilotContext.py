84b75d16db11019c70d0d6ff52941ea35e4ffe68
melionel <mengla@microsoft.com>
2023-07-22 13:34:53 +0800

    add copilot context and check env

---
+++ CopilotContext.py
@@ -0,0 +1,30 @@
+import os
+import json
+import openai
+import argparse
+from dotenv import load_dotenv
+from jinja2 import Environment, FileSystemLoader
+
+class CopilotContext:
+    def __init__(self) -> None:
+        load_dotenv('pfcopilot.env')
+        self.use_aoai = os.environ.get("AOAI_BY_DEFAULT", "true").lower() == "true"
+        self.aoai_key = os.environ.get("AOAI_API_KEY")
+        self.aoai_deployment = os.environ.get("AOAI_DEPLOYMENT")
+        self.aoai_api_base = os.environ.get("AOAI_API_BASE")
+
+        self.openai_key = os.environ.get("OPENAI_API_KEY")
+        self.openai_model = os.environ.get("OPENAI_API_KEY")
+
+    def check_env(self):
+        if self.use_aoai:
+            if not self.aoai_key or not self.aoai_deployment or not self.aoai_api_base:
+                return False, "You configured to use AOAI, but one or more of the following environment variables were not set: AOAI_API_KEY, AOAI_DEPLOYMENT, AOAI_API_BASE"
+            else:
+                return True, ""
+        else:
+            if not self.openai_key or not self.openai_model:
+                return False, "You configured to use OPENAI, but one or more of the following environment variables were not set: OPENAI_API_KEY, OPENAI_API_KEY"
+            else:
+                return True, ""
+            
--- main.py
+++ main.py
@@ -1,31 +1,43 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import Scrollbar
+from CopilotContext import CopilotContext
 
 welcome_message = """
 Copilot: Welcome to use promptflow copilot! I can help you to create a promptflow to accomplish your goal.
 Although the auto generated flow might be not right or lack of some implementation details, it should be a good start to develop your own flow. Enjoy!
 
 Before start, remember to set below variables in the pfcopilot.env file.
-All set? Let's start!
+"""
 
+checking_environment_message = "Checking your environment..."
+
+environment_ready_message = """
+Copilot: You are all set, let's start!
 Pleae tell me your goal you want to accomplish using promptflow.
+"""
+
+environment_not_ready_message = """
+Copilot: Your environment is not ready, please configure your environment and restart the window.
 """
 
 BG_GRAY = "#ABB2B9"
 BG_COLOR = "#17202A"
 TEXT_COLOR = "#EAECEE"
 PILOT_TEXT_COLOR = "#6495ED"
+
+USER_TAG = 'User'
+COPILOT_TAG = 'Copilot'
  
 FONT = "Helvetica 14"
 FONT_BOLD = "Helvetica 13 bold"
 
 def get_response():
     user_input = entry.get()
-    add_to_chat("You: " + user_input, 'User')
+    add_to_chat("You: " + user_input, USER_TAG)
     # Add your response logic here
     response = "Copilot: Thanks for your input!"
-    add_to_chat(response, 'Copilot')
+    add_to_chat(response, COPILOT_TAG)
     chat_box.yview_moveto(1.0)
 
 def add_to_chat(message, tag):
@@ -38,13 +50,16 @@ def add_to_chat(message, tag):
 app = tk.Tk()
 app.title("Promptflow Copilot")
 
+# init CopilotContext
+copilot_context = CopilotContext()
+
 # Create a text widget to display the chat conversation
 chat_box = tk.Text(app, width=100, bg=BG_COLOR, fg=PILOT_TEXT_COLOR, font=FONT)
 chat_box.grid(row=1, column=0, columnspan=2)
 chat_box.tag_config('User', background=BG_COLOR, foreground = TEXT_COLOR)
 chat_box.tag_config('Copilot',  background=BG_COLOR, foreground = PILOT_TEXT_COLOR)
 
-chat_box.insert(tk.END, welcome_message + "\n", 'Copilot')
+chat_box.insert(tk.END, welcome_message, COPILOT_TAG)
 chat_box.config(state=tk.DISABLED)
 # scrollbar = Scrollbar(chat_box)
 # scrollbar.place(relheight=1, relx=0.974)
@@ -58,6 +73,14 @@ button_style = ttk.Style()
 button_style.configure('TButton', font=('Helvetica', 12))
 button = ttk.Button(app, text="Send", command=get_response, style='TButton')
 button.grid(row=2, column=1)
+
+# check environment
+env_ready, msg = copilot_context.check_env()
+add_to_chat(checking_environment_message, COPILOT_TAG)
+if env_ready:
+    add_to_chat(environment_ready_message, COPILOT_TAG)
+else:
+    add_to_chat(environment_not_ready_message + msg + '\n', COPILOT_TAG)
 
 # Run the main event loop
 app.mainloop()
